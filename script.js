a636117287e541af485257bb575f2bb64f4b2b66
Izabelle Alves <izabelle.alvesbl@gmail.com>
2025-03-18 23:37:43 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const iniciar = document.querySelector(".iniciar");
+const pausar = document.querySelector(".pausar");
+const tempo = document.querySelector(".tempo");
+const reiniciar = document.querySelector(".reiniciar");
+
+iniciar.addEventListener("click", iniciarTempo);
+pausar.addEventListener("click", pausarTempo);
+reiniciar.addEventListener("click", reiniciarTempo); //dbl -> dar dois cliques pra ocorrer
+
+let i = 0;
+let timer;
+
+function iniciarTempo() {
+  timer = setInterval(() => {
+    const segundos = i % 60;
+    const minutos = Math.floor(i / 60) % 60;
+    const horas = Math.floor(i / 3600);
+
+    tempo.innerText = `${horas.toString().padStart(2, "0")}:${minutos
+      .toString()
+      .padStart(2, "0")}:${segundos.toString().padStart(2, "0")}`;
+    i++;
+  }, 100);
+  iniciar.setAttribute("disabled", "");
+  pausar.removeAttribute("disabled", "");
+  reiniciar.classList.add("hide");
+}
+
+function pausarTempo() {
+  clearInterval(timer);
+  iniciar.removeAttribute("disabled", "");
+  pausar.setAttribute("disabled", "");
+  iniciar.innerText = "Continuar";
+  reiniciar.classList.remove("hide");
+}
+
+function reiniciarTempo() {
+  reiniciar.classList.toggle("hide");
+  iniciar.innerText = "Iniciar";
+  tempo.innerText = 0;
+  i = 0;
+}
